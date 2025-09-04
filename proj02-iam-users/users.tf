77f7b033f5474304c4e6dcaa3af605739061b67f
Aitor <aitorconesalucena@gmail.com>
2025-09-04 19:34:13 +0200

    proj02-iam

---
+++ proj02-iam-users/users.tf
@@ -0,0 +1,35 @@
+locals {
+  users_from_yaml = yamldecode(file("${path.module}/user-roles.yaml")).users
+}
+
+resource "aws_iam_user" "users" {
+  for_each = toset(local.users_from_yaml[*].username)
+  name     = each.value
+}
+
+resource "aws_iam_user_login_profile" "users" {
+  for_each        = aws_iam_user.users
+  user            = each.value.name
+  password_length = 8
+
+
+  lifecycle {
+    ignore_changes = [
+      password_length,
+      password_reset_required,
+      pgp_key
+    ]
+  }
+}
+
+output "passwords" {
+  sensitive = true
+  value = { 
+    for user, user_login in aws_iam_user_login_profile.users : user => user_login.password
+    }
+}
+
+output "users" {
+  value = local.users_from_yaml
+}
+
