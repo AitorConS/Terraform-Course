dfd7b8425cb13cdbc144e93f9c4c70a86e541479
Aitor Conesa <aitorconesalucena@gmail.com>
2025-09-10 13:31:36 +0200

    using own module

---
+++ 15-use-own-module/networking.tf
@@ -0,0 +1,20 @@
+module "networking-tf-course" {
+  source  = "AitorConS/networking-tf-course/aws"
+  version = "0.1.1"
+  vpc_config = {
+    cidr_block = "10.0.0.0/16"
+    name       = "14-local-modules"
+  }
+
+  subnet_config = {
+    subnet_1 = {
+      cidr_block = "10.0.0.0/24"
+      az         = "eu-west-1a"
+    }
+    subnet_2 = {
+      cidr_block = "10.0.2.0/24"
+      public     = true
+      az         = "eu-west-1b"
+    }
+  }
+}
