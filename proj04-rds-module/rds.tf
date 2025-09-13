53d7a9b1b6a346961f9658be3b14d37e23e28a0e
Aitor Conesa <aitorconesalucena@gmail.com>
2025-09-14 01:17:09 +0200

    Proj04-Validations-RDS

---
+++ proj04-rds-module/rds.tf
@@ -0,0 +1,21 @@
+module "database" {
+  source = "./modules/rds"
+
+  project_name = "proj04-rds-module"
+
+  storage_size = 10
+
+  credentials = {
+    username = "db-admin"
+    password = "abc123?"
+  }
+
+  security_groups_ids = [
+
+  ]
+
+  subnet_ids = [
+    aws_subnet.allowed.id
+  ]
+
+}
